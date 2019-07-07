6f8e4d2f037e123bf9510af0591d3905e5b91589
Nilesh <nlshnk95@gmail.com>
2019-07-08 00:06:43 +0530

    side hamburger menu design implementation

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <div class="main-sidebar" :class="{ active: isVisible }">
+      <ul class="lists">
+        <li v-for="item in sidebarList" v-bind:key="item.id">
+          <router-link to>{{ item.listName }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="hamburger-menu" @click="slideInOut" :class="{ active: isVisible }">
+      <span></span>
+      <br />
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      isVisible: false,
+      sidebarList: [
+        { listName: "Home" },
+        { listName: "About" },
+        { listName: "Skills" },
+        { listName: "Projects" },
+        { listName: "Conatct Me" }
+      ]
+    };
+  },
+  methods: {
+    slideInOut: function() {
+      this.isVisible = !this.isVisible;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.main-sidebar {
+  position: fixed;
+  left: 0;
+  top: 0;
+  background: linear-gradient(to right, #5918df, #7733ff);
+  height: 100vh;
+  width: 100%;
+  transform: translateX(-100%);
+  transition: transform 0.8s ease-in-out;
+  &.active {
+    transform: translateX(0);
+  }
+  ul {
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    li {
+      padding: 28px 0;
+      a {
+        color: #fff;
+        font-size: 32px;
+        font-weight: 300;
+        text-transform: uppercase;
+        &:hover {
+          color: #fff;
+        }
+      }
+    }
+  }
+}
+.hamburger-menu {
+  position: fixed;
+  left: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  span {
+    display: inline-block;
+    width: 46px;
+    height: 1px;
+    background: linear-gradient(to right, #5918df, #7733ff);
+    transition: all 0.3s linear;
+  }
+  &.active {
+    span {
+      background: linear-gradient(rgb(255, 255, 255),rgb(235, 235, 235));
+      &:first-child {
+        transform: rotate(45deg);
+      }
+      &:last-child {
+        transform: translateY(-19px) rotate(-45deg);
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// material ui framework
+import { MdButton } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+// Default css
+import "./assets/css/style.scss";
 
+Vue.use(MdButton);
 Vue.config.productionTip = false;
 
 new Vue({
